e21593e28710ccd6470ca770b269d4a53b103ec3
goutam-akash <mgoutam1999@gmail.com>
2024-07-22 22:40:34 -0400

    updated server.js 0.0.0.0

--- translation-backend/server.js
+++ translation-backend/server.js
@@ -42,6 +42,6 @@ app.post('/api/translations', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
