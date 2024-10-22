284f88023d048fe1d039ea92d80a04f142161b5c
goutam-akash <mgoutam1999@gmail.com>
2024-10-22 01:33:37 -0400

    minor change v2

--- src/App.jsx
+++ src/App.jsx
@@ -130,7 +130,7 @@ const App = () => {
 
       // Send translation result to the backend
       await fetch(
-        "https://translation-app-ooq8.onrender.com",
+        "https://translation-app-ooq8.onrender.com/api/translations",
         {
           method: "POST",
           headers: {
