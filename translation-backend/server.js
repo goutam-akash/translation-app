060f8f3e2dc74040fc40a72418217cd6f76d06ef
goutam-akash <mgoutam1999@gmail.com>
2024-12-02 00:55:57 -0500

    changed db

--- translation-backend/server.js
+++ translation-backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { Pool } = require('pg');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { Pool } = require("pg");
+const bodyParser = require("body-parser");
 const { Parser } = require("json2csv");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,21 +14,21 @@ app.use(express.json());
 
 // Database connection
 const pool = new Pool({
-  user: "my_postgres_mda8_user",
-  host: "dpg-cs30bn9u0jms7391lq5g-a.oregon-postgres.render.com",
-  database: "my_postgres_mda8",
-  password: "viFQtpBhnVhEwyB2XZh8qXtNPNrrauTj",
-  port: 5432,
-  idleTimeoutMillis: 30000,  // close idle clients after 30 seconds
-  connectionTimeoutMillis: 5000,  // wait for a maximum of 5 seconds for a connection
-  ssl: {
-    rejectUnauthorized: false // Allows self-signed certificates
-  },
+    user: "my_postgres_dqrl_user",
+    host: "dpg-ct6kl1dumphs739h8ga0-a.oregon-postgres.render.com",
+    database: "my_postgres_dqrl",
+    password: "kVesnrcNJAslyzXoCfUdess3b6OJ74cm",
+    port: 5432,
+    idleTimeoutMillis: 30000, // close idle clients after 30 seconds
+    connectionTimeoutMillis: 5000, // wait for a maximum of 5 seconds for a connection
+    ssl: {
+        rejectUnauthorized: false, // Allows self-signed certificates
+    },
 });
 
 // Function to create the table if it doesn't exist
 const createTableIfNotExists = async () => {
-  const createTableQuery = `
+    const createTableQuery = `
     CREATE TABLE IF NOT EXISTS translations (
       id SERIAL PRIMARY KEY,
       original_message TEXT NOT NULL,
@@ -39,55 +39,54 @@ const createTableIfNotExists = async () => {
     );
   `;
 
-  try {
-    await pool.query(createTableQuery);
-    console.log('Table "translations" is ready.');
-  } catch (error) {
-    console.error('Error creating table:', error);
-  }
+    try {
+        await pool.query(createTableQuery);
+        console.log('Table "translations" is ready.');
+    } catch (error) {
+        console.error("Error creating table:", error);
+    }
 };
 
 // Create the table when the server starts
 createTableIfNotExists();
 
 // Route for handling POST requests
-app.post('/api/translations', async (req, res) => {
-  const { original_message, translated_message, language, model } = req.body;
-  if (!original_message || !translated_message || !language || !model) {
-    res.status(400).json({ error: 'Missing required fields' });
-    return;
-  }
+app.post("/api/translations", async (req, res) => {
+    const { original_message, translated_message, language, model } = req.body;
+    if (!original_message || !translated_message || !language || !model) {
+        res.status(400).json({ error: "Missing required fields" });
+        return;
+    }
 
-  try {
-    const result = await pool.query(
-      'INSERT INTO translations (original_message, translated_message, language, model) VALUES ($1, $2, $3, $4) RETURNING *',
-      [original_message, translated_message, language, model]
-    );
-    res.status(201).json(result.rows[0]);
-  } catch (error) {
-    console.error('Database insertion error:', error);
-    res.status(500).json({ error: 'Database insertion error' });
-  }
+    try {
+        const result = await pool.query(
+            "INSERT INTO translations (original_message, translated_message, language, model) VALUES ($1, $2, $3, $4) RETURNING *",
+            [original_message, translated_message, language, model]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error("Database insertion error:", error);
+        res.status(500).json({ error: "Database insertion error" });
+    }
 });
 
 app.get("/api/export", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM translations"); // Modify query as needed
-    const jsonData = result.rows;
+    try {
+        const result = await pool.query("SELECT * FROM translations"); // Modify query as needed
+        const jsonData = result.rows;
 
-    const json2csvParser = new Parser();
-    const csv = json2csvParser.parse(jsonData);
+        const json2csvParser = new Parser();
+        const csv = json2csvParser.parse(jsonData);
 
-    res.header("Content-Type", "text/csv");
-    res.attachment("output_file.csv");
-    res.send(csv);
-  } catch (error) {
-    console.error("Error exporting to CSV:", error);
-    res.status(500).send("Internal Server Error");
-  }
+        res.header("Content-Type", "text/csv");
+        res.attachment("output_file.csv");
+        res.send(csv);
+    } catch (error) {
+        console.error("Error exporting to CSV:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
-
