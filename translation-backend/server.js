377f0196dc511e82635e8bdc11101553f05e347e
goutam-akash <42815005+goutam-akash@users.noreply.github.com>
2024-12-03 22:40:11 -0500

    Update server.js

--- translation-backend/server.js
+++ translation-backend/server.js
@@ -37,7 +37,7 @@ const createTableIfNotExists = async () => {
     model VARCHAR(50) NOT NULL,
     ranking INT DEFAULT 0,
     rating FLOAT CHECK (rating BETWEEN 0 AND 5) DEFAULT 0,
-    classification VARCHAR(50) ,
+    classification VARCHAR(50)  DEFAULT translation ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     `;
